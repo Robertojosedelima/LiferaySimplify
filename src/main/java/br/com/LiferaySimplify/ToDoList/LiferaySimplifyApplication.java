d59ccff472ac533a1e3f3a1bbbc230d420be46dd
Roberto lima <drop1108@gmail.com>
2024-02-03 01:02:08 -0300

    first commit .

---
+++ src/main/java/br/com/LiferaySimplify/ToDoList/LiferaySimplifyApplication.java
@@ -0,0 +1,13 @@
+package br.com.LiferaySimplify.ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiferaySimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiferaySimplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/LiferaySimplify/ToDoList/LiferaySimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.LiferaySimplify.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiferaySimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
